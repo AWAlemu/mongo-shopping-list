5f939b7ea4e48b2aff31cd31fa1630e639941cb4
Alex <alexweldetsadik@gmail.com>
2016-09-08 19:57:55 +0000

    implemented continuous deployment using Travis CI

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
                             'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev');
+                            'mongodb://<awalemu>:<57d09c17ee09c21e3cd9ae6f>@ds019906.mlab.com:19906/contdeployment');
 exports.PORT = process.env.PORT || 8080;
